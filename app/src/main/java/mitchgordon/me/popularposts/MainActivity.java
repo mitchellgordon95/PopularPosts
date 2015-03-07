d058b18c0c0b799d4e9079b94e19a663ce4ce0e4
Mitchell Gordon <mitchell.gordon95@gmail.com>
2015-03-07 03:36:59 -0600

    Basic functionality (if access tokens behave)

--- app/src/main/java/mitchgordon/me/popularposts/MainActivity.java
+++ app/src/main/java/mitchgordon/me/popularposts/MainActivity.java
@@ -1,6 +1,7 @@
 package mitchgordon.me.popularposts;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -11,15 +12,30 @@ import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.facebook.Request;
+import com.facebook.Response;
 import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphObject;
+import com.facebook.model.GraphObjectList;
+import com.facebook.widget.LoginButton;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import rx.Observable;
+import rx.schedulers.Schedulers;
 
 
 public class MainActivity extends Activity {
@@ -29,19 +45,95 @@ public class MainActivity extends Activity {
     @InjectView(R.id.titleTV)
     TextView titleTV;
 
+    @InjectView(R.id.loginButton)
+    LoginButton authButton;
+
+    @InjectView(R.id.postsLV)
+    ListView postsLV;
+
+    public class Post implements Comparable<Post> {
+        int likes;
+        String message;
+
+        public Post(int likes, String message) {
+            this.likes = likes;
+            this.message = message;
+        }
+
+        public int compareTo(Post other) {
+            return other.likes - this.likes;
+        }
+
+        public String toString() {
+            return message + "\n\n" + likes;
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
 
+        authButton.setReadPermissions("read_stream");
+
         uiHelper = new UiLifecycleHelper(this, (session, state, exception) -> {
             if(session.isOpened()) {
+                Log.d("PopularPosts", session.getAccessToken());
                 // Grab the posts and display them.
+                Observable<Post> postsStream = Observable.create( subscriber -> {
+                    Request request = Request.newGraphPathRequest(session, "/me/feed?fields=likes.limit(1).summary(true),message", response -> {});
+                    while(request != null) {
+                        Response resp = Request.executeBatchAndWait(request).get(0);
+                        Log.d("PopularPosts", "Got a response.");
+                        if(resp.getError() != null)
+                            subscriber.onError((Throwable)resp.getError().getException());
+
+                        GraphObjectList<GraphObject> posts =
+                                resp.getGraphObject().getPropertyAsList("data", GraphObject.class);
+
+                        for (GraphObject post : posts ) {
+                            JSONObject postJSON = post.getInnerJSONObject();
+
+                            int likes;
+                            try {
+                                likes = postJSON.getJSONObject("likes").getJSONArray("data").length();
+                            }
+                            catch(Exception e) { likes = 0;};
+                            String msg;
+                            try {
+                                msg = postJSON.getString("message");
+                            }
+                            catch(Exception e) { msg = ""; }
+                            subscriber.onNext(new Post(likes, msg));
+                        }
+
+                        request = resp.getRequestForPagedResults(Response.PagingDirection.NEXT);
+                    }
+
+                    Log.d("PopularPosts", "Finished");
+                    subscriber.onCompleted();
+                });
+
+                postsStream
+                .toSortedList()
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(sortedPosts -> {
+                    runOnUiThread( () -> {
+                                titleTV.setVisibility(View.GONE);
+                                postsLV.setVisibility(View.VISIBLE);
+                                postsLV.setAdapter(new ArrayAdapter<Post>(getApplicationContext(),
+                                        R.layout.simple_list_item,
+                                        sortedPosts.toArray(new Post[sortedPosts.size()])));
+                            } );
+                },
+                e -> Log.e("PopularPosts", e.getMessage()));
             }
             else {
                 // Show the title and hide the posts
-
+                titleTV.setVisibility(View.VISIBLE);
+                postsLV.setVisibility(View.INVISIBLE);
             }
         });
 
